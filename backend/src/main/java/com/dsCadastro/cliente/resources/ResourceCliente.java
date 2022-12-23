b6a79e9ae405016b28e76e23660fd002c10e0e37
Patrick Pereira <patrick.pereira27@hotmail.com>
2022-12-23 08:02:06 -0300

    finalização projeto de fechamento modulo 1

--- backend/src/main/java/com/dsCadastro/cliente/resources/ResourceCliente.java
+++ backend/src/main/java/com/dsCadastro/cliente/resources/ResourceCliente.java
@@ -1,14 +1,22 @@
 package com.dsCadastro.cliente.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dsCadastro.cliente.dto.ClientDTO;
 import com.dsCadastro.cliente.service.ClientService;
@@ -33,11 +41,48 @@ public class ResourceCliente {
 		//direction    = tipo de ordenação ASC ou DESC
 		//orderBy 	   = define o nome que ordenara os dados
 		
-		
 		PageRequest pageRequest = PageRequest.of(page,linesPerPage,Direction.valueOf(direction),orderBy);
 		Page<ClientDTO> list = service.findAllPaged(pageRequest);
 				
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+		
+		ClientDTO client = service.findById(id);
+		
+		return ResponseEntity.ok().body(client);
+	}
+	
+	//salva e retorna o cliente salvo
+	@PostMapping()
+	public ResponseEntity<ClientDTO> save(@RequestBody ClientDTO dto){	
+		dto = service.save(dto);
+		
+		//comando para formar o location com o recurso criado, que seria o clients/id o id será do novo objeto salvo
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		
+		//retorna 201 pra falar que o recurso foi criado, e também retorna o location por conta de boas praticas
+		return ResponseEntity.created(uri).body(dto);
+		
+	}
+	
+	//atualiza o cadastro de cliente
+	@PutMapping("/{id}")
+	public ResponseEntity<ClientDTO> update(@RequestBody ClientDTO dto, @PathVariable Long id){	
+		
+		dto = service.update(id,dto);
+		return ResponseEntity.ok().body(dto);
+	}	
+	
+	//método para excluir cliente
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> delete( @PathVariable Long id){	
+		service.delete(id);
+		return ResponseEntity.noContent().build(); //retorna 204 - quer dizer que deu certo e o corpo da resposta está vazio
+	}
+	
+	
 
 }
--- backend/src/main/java/com/dsCadastro/cliente/service/ClientService.java
+++ backend/src/main/java/com/dsCadastro/cliente/service/ClientService.java
@@ -1,6 +1,12 @@
 package com.dsCadastro.cliente.service;
 
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -9,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.dsCadastro.cliente.dto.ClientDTO;
 import com.dsCadastro.cliente.entities.Client;
 import com.dsCadastro.cliente.repository.ClientRepository;
+import com.dsCadastro.cliente.service.exception.DatabaseException;
+import com.dsCadastro.cliente.service.exception.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -16,12 +24,70 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	//método que retorna os dados de todas as categorias páginados
+	//método que retorna os dados de todas os clientes
 	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
 		
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(cli -> new ClientDTO(cli)); //cada cli é um objeto da lista retornada do banco
 	}
+	
+	//método para retornar o cliente com o id mencionado
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		 Optional<Client> obj = repository.findById(id);
+		 Client client = obj.orElseThrow(() -> new ResourceNotFoundException(String.format("Cliente com id %s não localizado.", id)));
+		
+		return new ClientDTO(client);
+	}
+	
+	//método para salvar um novo cliente
+	@Transactional
+	public ClientDTO save(ClientDTO dto) {
+		Client client = new Client();
+		copyDtoToEntity(dto,client);
+		client = repository.save(client);
+		return new ClientDTO(client);
+	}
+	
+	//método para atualizar cadastro de cliente
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client client =  repository.getOne(id); 
+			copyDtoToEntity(dto, client);
+			client = repository.save(client);
+			return new ClientDTO(client);
+			
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(String.format("ID %s não localizado para atualização.", id));
+		}
+	}
+	
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);			
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(String.format("ID %s não localizado para remoção.", id));
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(String.format("Cliente com id %s não pode ser removido, possui vinculo com entidades relacionadas.", id));
+		}
+		
+	}
+	
+
+	//métodos auxiliares
+	private void copyDtoToEntity(ClientDTO dto, Client client) {
+		client.setBirthDate(dto.getBirthDate());
+		client.setChildren(dto.getChildren());
+		client.setCpf(dto.getCpf());
+		client.setIncome(dto.getIncome());
+		client.setName(dto.getName());
+	}
+
+
+
+	
 
 }
